1b60e710cd146ee3d111bfd193d3f9510d7f5445
Alyssa Daniels <asagedaniels@gmail.com>
2024-06-21 13:04:24 -0500

    added navbar and example for data retrieval

---
+++ src/app/example/page.tsx
@@ -0,0 +1,26 @@
+'use client'
+import { useEffect, useState } from "react";
+
+export default function Home() {
+    const [data, setData] = useState(null)
+    const [isLoading, setLoading] = useState(true)
+   
+    useEffect(() => {
+      fetch('http://127.0.0.1:8000/workshop/')
+        .then((res) => res.json())
+        .then((data) => {
+          setData(data)
+          setLoading(false)
+        })
+        .catch((e) => console.log(e))
+    }, [])
+   
+    if (isLoading) return <p>Loading...</p>
+    if (!data) return <p>No data</p>
+   
+    return (
+      <div>
+        {data.map((item) => <div>{item.fields.workshop_title}</div>)}
+      </div>
+    )
+  }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,6 @@
 import BackgroundImage from "@/components/BackgroundImage";
 import CountdownTimer from "@/components/CountdownTimer";
+import Navbar from "@/components/Navbar"
 
 const BACKGROUND_URL = "/welcome-ceremony.jpg";
 const BACKGROUND_ALT = "FACT 2023 Welcome Ceremony";
@@ -8,6 +9,7 @@ export default function Home() {
   return (
     <>
       <BackgroundImage imageURL={BACKGROUND_URL}>
+        <Navbar/>
         <div className="flex flex-col justify-center text-center">
           <div className="italic text-4xl">MAGBUBUNGA</div>
           <div className="font-bold text-9xl my-6">FACT 2024</div>
