1c8220987830da69be12577316fc51c1dbe6d75f
rossebubless <danzemil@icloud.com>
2024-07-18 13:47:25 -0500

    added navbar to agenda page

---
+++ src/app/agenda/page.tsx
@@ -0,0 +1,98 @@
+"use client";
+import Navbar from "@/components/Navbar";
+import Button from "@/components/Button";
+import { useState } from "react";
+
+const agendaData = [
+    {
+        day: "Friday mm/dd",
+        info: [
+            {
+                id: "0",
+                event: "Event Name",
+                time: "00:00AM - 00:00 AM",
+                location: "Building Name #",
+            },
+            {
+                id: "1",
+                event: "Event Name",
+                time: "00:00AM - 00:00 AM",
+                location: "Building Name #",
+            },
+        ],
+    },
+    {
+        day: "Saturday mm/dd",
+        info: [
+            {
+                id: "0",
+                event: "Event Name",
+                time: "00:00AM - 00:00 AM",
+                location: "Building Name #",
+            },
+            {
+                id: "1",
+                event: "Event Name",
+                time: "00:00AM - 00:00 AM",
+                location: "Building Name #",
+            },
+            {
+                id: "2",
+                event: "Event Name",
+                time: "00:00AM - 00:00 AM",
+                location: "Building Name #",
+            },
+            {
+                id: "3",
+                event: "Event Name",
+                time: "00:00AM - 00:00 AM",
+                location: "Building Name #",
+            },
+        ],
+    },
+];
+
+export default function Agenda() {
+    const [activeTab, setActiveTab] = useState(0);
+
+    return (
+        <>
+            <Navbar />
+            <div className="text-center p-10 border-b-2 m-10">
+                <div className="font-bold sm:text-4xl lg:text-6xl">AGENDA</div>
+            </div>
+            <div className="w-2/4 mx-auto mb-4">
+                <div className="flex flex-row justify-start">
+                    {agendaData.map((item, index) => (
+                        <div
+                            className={
+                                "px-4 py-3 rounded-t-lg cursor-pointer" +
+                                (index == activeTab
+                                    ? " bg-text-primary text-background-primary"
+                                    : "")
+                            }
+                            key={item.day}
+                            onClick={() => setActiveTab(index)}
+                        >
+                            <div className="sm:text-3xl lg:text-4xl font-bold">
+                                {item.day}
+                            </div>
+                        </div>
+                    ))}
+                </div>
+                <div className="border-t-2 p-2">
+                    {agendaData[activeTab].info.map((item) => (
+                        <div className="mb-4 text-xl" key={item.id}>
+                            <div className="font-bold">{item.event}</div>
+                            <div>{item.time}</div>
+                            <div>{item.location}</div>
+                        </div>
+                    ))}
+                </div>
+            </div>
+            <div className="w-fit m-auto">
+                <Button text="REGISTER NOW" url="/" />
+            </div>
+        </>
+    );
+}
