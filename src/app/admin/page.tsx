02893fea0ca605f95e4d7b0cfc02f424f8485eb1
rossebubless <danzemil@icloud.com>
2024-08-21 13:27:21 -0500

    formatted dashboard

--- src/app/admin/page.tsx
+++ src/app/admin/page.tsx
@@ -8,9 +8,11 @@ export default function Admin() {
                 <div className="font-bold sm:text-4xl lg:text-6xl m-10 pb-4 w-fit border-b-4">FACT Admin Dashboard</div>
                 <div className="flex flex-row justify-evenly my-8">
                     <div className="flex flex-col">
-                        <Button text="Update Individual Workshop" url="/" /> 
-                        <Button text="Upload Workshops" url="/" /> 
-                        <Button text="Upload Locations" url="/" /> 
+                        <ul>
+                            <li>Update Individual Workshop</li>
+                            <li>Upload Workshops</li>
+                            <li>Upload Locations</li>
+                        </ul>
                     </div>
                     <div className="flex flex-col justify-evenly items-center bg-text-primary py-4 px-2">
                         <div className="font-bold text-black text-2xl">STATS</div>
