2abb541e4dc4a4fdc41ff4cca0e8c67df1e4a6a2
rossebubless <danzemil@icloud.com>
2024-08-30 16:41:12 -0500

    finished carousel component, added image carousels to gallery page

--- src/components/ui/Carousel.tsx
+++ src/components/ui/Carousel.tsx
@@ -1,5 +1,6 @@
 "use client";
 import { useState } from "react";
+import { IoIosArrowForward, IoIosArrowBack } from "react-icons/io";
 
 interface CarouselProps {
     title: string;
@@ -12,23 +13,26 @@ export default function Carousel({title, images}: CarouselProps) {
     return(
         <>
             <div className="sm:text-3xl lg:text-4xl">{title}</div>
-            <div className="flex flex-row justify-between my-8">
-                <div className="size-10 bg-white cursor-pointer self-center" 
+            <div className="flex flex-row justify-between my-8 h-64">
+                <div className="size-9 cursor-pointer self-center text-4xl" 
                     onClick={()=>setCurrentIndex(() => currentIndex - 3 < 0 ? ~~((images.length - 1) / 3) * 3 : currentIndex - 3)}>
-
+                        <IoIosArrowBack />
                 </div>
-                <div className="w-1/4 h-1/6 bg-white">
-                    <img src={images[currentIndex]} className="b-1"/>
+                <div className="h-64 w-96">
+                    <img key={currentIndex} src={images[currentIndex]} className="b-1"/>
                 </div>
-                <div className="w-1/4 h-1/6 bg-white">
-                    <img src={images[(currentIndex + 1 > images.length ? currentIndex : currentIndex + 1)]} 
+                <div className="h-64 w-96">
+                    <img key={currentIndex+1} src={images[(currentIndex + 1 > images.length ? currentIndex : currentIndex + 1)]} 
                         className={"b-1" + (currentIndex + 1 > images.length ? " hidden" : "")}/>
                 </div>
-                <div className="w-1/4 h-1/6 bg-white">
-                    <img src={images[(currentIndex + 2 > images.length ? currentIndex : currentIndex + 2)]} 
+                <div className="h-64 w-96">
+                    <img key={currentIndex+2} src={images[(currentIndex + 2 > images.length ? currentIndex : currentIndex + 2)]} 
                         className={"b-1" + (currentIndex + 2 > images.length ? " hidden" : "")}/>
                 </div>
-                <div className="size-10 bg-white cursor-pointer self-center" onClick={()=>setCurrentIndex(() => currentIndex + 3 > images.length - 1 ? 0 : currentIndex + 3)}></div>
+                <div className="size-9 cursor-pointer self-center text-4xl" 
+                    onClick={()=>setCurrentIndex(() => currentIndex + 3 > images.length - 1 ? 0 : currentIndex + 3)}>
+                    <IoIosArrowForward />
+                </div>
             </div>
         </>
     );
