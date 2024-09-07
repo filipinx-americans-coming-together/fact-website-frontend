3c5dc84580cef0b8e7815ff27ca12cbed0b504cd
Alyssa Daniels <asagedaniels@gmail.com>
2024-09-07 14:41:25 -0500

    Fixed desktop hover bug

--- src/components/navigation/DesktopNav.tsx
+++ src/components/navigation/DesktopNav.tsx
@@ -4,9 +4,9 @@ export default function DesktopNav(props: {
     links: { text: string; path: string }[];
 }) {
     return (
-        <ul className="hidden text-text-primary hover:text-highlight-primary items-center justify-center text-center gap-16 md:flex">
+        <ul className="hidden items-center justify-center text-center gap-16 md:flex">
             {props.links.map((link, index) => (
-                <li key={index}>
+                <li className="text-text-primary hover:text-highlight-primary" key={index}>
                     <Link href={link.path} prefetch={true}>
                         {link.text}
                     </Link>
