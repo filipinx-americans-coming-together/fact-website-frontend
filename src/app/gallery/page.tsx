c5daf726c6265060e271989552bfcbd9278e44ce
rossebubless <danzemil@icloud.com>
2024-09-10 14:27:35 -0500

    fixed reflexive gallery

--- src/app/gallery/page.tsx
+++ src/app/gallery/page.tsx
@@ -1,43 +1,24 @@
 "use client";
 import PageContainer from "@/components/formatting/PageContainer";
-import SingleCarousel from "@/components/ui/SingleCarousel";
-import DoubleCarousel from "@/components/ui/DoubleCarousel";
-import TripleCarousel from "@/components/ui/TripleCarousel";
-import { useEffect, useState } from "react";
+import Carousel from "@/components/ui/Carousel";
+import { useMediaQuery } from "@uidotdev/usehooks";
 
 
 export default function Gallery() {
-    if (typeof window !== "undefined") {
-    const [numImages, setNumImages] = useState(window.innerWidth < 640 ? 1 : (window.innerWidth < 1024 ? 2 : 3));
-    useEffect(() => {
-        const handleResize = async () => {
-            const newNum = window.innerWidth < 640 ? 1 : (window.innerWidth < 1024 ? 2 : 3);
-            console.log(window.innerWidth, newNum);
-            if (newNum !== numImages) {
-                setNumImages(newNum);
-                // console.log(window.innerWidth);
-                // console.log(numImages);
-            }
-        };
-    
-        window.addEventListener('resize', handleResize);
-
-        return () => {window.removeEventListener('resize', handleResize);};
-      }, []);
-
-    // useEffect(() => {console.log("rerender");}, [numImages]);
+    const isSmallDevice = useMediaQuery("only screen and (max-width : 640px)");
+    const isMediumDevice = useMediaQuery(
+        "only screen and (min-width : 641px) and (max-width : 1024px)"
+    );
 
     return (
         <PageContainer title="Gallery">
-            <p>{numImages}</p>
-            {numImages == 1 ? <SingleCarousel title="Welcome Ceremony" src="welcome-ceremony" length={25}/> : (numImages == 2 ? <DoubleCarousel title="Welcome Ceremony" src="welcome-ceremony" length={25}/> : <TripleCarousel title="Welcome Ceremony" src="welcome-ceremony" length={25}/>)}
+            <Carousel title="Welcome Ceremony" src="welcome-ceremony" length={25} numImages={isSmallDevice ? 1 : (isMediumDevice ? 2 : 3)} />
             <div className="border-b-2 h-4 w-full mb-4"></div>
-            {numImages == 1 ? <SingleCarousel title="Workshops" src="workshops" length={11}/> : (numImages == 2 ? <DoubleCarousel title="Workshops" src="workshops" length={11}/> : <TripleCarousel title="Workshops" src="workshops" length={11}/>)}
+            <Carousel title="Workshops" src="workshops" length={11} numImages={isSmallDevice ? 1 : (isMediumDevice ? 2 : 3)} />
             <div className="border-b-2 h-4 w-full mb-4"></div>
-            {numImages == 1 ? <SingleCarousel title="Palengke" src="palengke" length={10}/> : (numImages == 2 ? <DoubleCarousel title="Palengke" src="palengke" length={10}/> : <TripleCarousel title="Palengke" src="palengke" length={10}/>)}
+            <Carousel title="Palengke" src="palengke" length={10} numImages={isSmallDevice ? 1 : (isMediumDevice ? 2 : 3)} />
             <div className="border-b-2 h-4 w-full mb-4"></div>
-            {numImages == 1 ? <SingleCarousel title="Variety Show" src="variety-show" length={28}/> : (numImages == 2 ? <DoubleCarousel title="Variety Show" src="variety-show" length={28}/> : <TripleCarousel title="Variety Show" src="variety-show" length={28}/>)}
+            <Carousel title="Variety Show" src="variety-show" length={28} numImages={isSmallDevice ? 1 : (isMediumDevice ? 2 : 3)} />
         </PageContainer>
     );
-    };
 }
