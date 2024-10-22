88a481c2f3a9d98d515a69d9b99286bc0f055b69
Alyssa Daniels <asagedaniels@gmail.com>
2024-10-22 16:11:13 -0500

    added workshop registration to facilitator dashboard

---
+++ src/app/facilitators/components/FacilitatorRegistration.tsx
@@ -0,0 +1,37 @@
+import InteractiveButton from "@/components/ui/InteractiveButton";
+import FacilitatorRow from "./FacilitatorRow";
+
+export default function FacilitatorRegistration({
+    facilitators,
+    facilitatedSessions,
+}: {
+    facilitators: string[];
+    facilitatedSessions: number[];
+}) {
+    return (
+        <>
+            <h1 className="font-bold text-xl">Register for workshops</h1>
+            <p className="text-xs">
+                Registration for other workshops is{" "}
+                <span className="font-bold">not</span> required for facilitators
+            </p>
+            <br />
+
+            {/* facilitator list */}
+            <div className="rounded p-6 flex flex-col gap-4 bg-text-primary text-background-primary">
+                {facilitators.map((facilitator) => {
+                    return (
+                        <FacilitatorRow
+                            name={facilitator}
+                            facilitatedSessions={facilitatedSessions}
+                        />
+                    );
+                })}
+                <br />
+                <div className="flex justify-center">
+                    <InteractiveButton text="Save Changes" onClick={() => {}} />
+                </div>
+            </div>
+        </>
+    );
+}
