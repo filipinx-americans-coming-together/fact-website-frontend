a994c7c0a5677516133873d3e789e1ac70f0c6ec
Alyssa Daniels <asagedaniels@gmail.com>
2024-10-25 21:58:11 -0500

    moved facilitator login to my-fact login

--- src/app/my-fact/login/page.tsx
+++ src/app/my-fact/login/page.tsx
@@ -9,19 +9,58 @@ import { useState } from "react";
 export default function Login() {
     const { login, isPending, error, isSuccess } = useLogin();
 
+    const [isDelegate, setIsDelegate] = useState(true);
+
     const [formData, setFormData] = useState<Object>({
         email: "",
         password: "",
     });
 
     if (isSuccess) {
-        window.location.href = "/my-fact/dashboard";
+        if (isDelegate) {
+            window.location.href = "/my-fact/dashboard";
+        } else {
+            window.location.href = "/facilitators/dashboard";
+        }
     }
 
     return (
         <>
             <Navbar />
 
+            <br />
+            {/* toggle log in type */}
+            <div className="mx-auto p-2 border-2 border-highlight-primary rounded w-fit flex gap-4">
+                <button
+                    type="button"
+                    className={
+                        (isDelegate
+                            ? "bg-highlight-primary text-background-primary rounded"
+                            : "") + " py-2 px-4"
+                    }
+                    onClick={(event) => {
+                        event.preventDefault();
+                        setIsDelegate(true);
+                    }}
+                >
+                    Delegate
+                </button>
+                <button
+                    type="button"
+                    className={
+                        (!isDelegate
+                            ? "bg-highlight-primary text-background-primary rounded"
+                            : "") + " py-2 px-4"
+                    }
+                    onClick={(event) => {
+                        event.preventDefault();
+                        setIsDelegate(false);
+                    }}
+                >
+                    Facilitator
+                </button>
+            </div>
+            <br />
             <FormContainer
                 submitText="Log in"
                 formName="loginForm"
@@ -31,7 +70,9 @@ export default function Login() {
                 isLoading={isPending}
                 errorMessage={error?.message}
             >
-                <div className="text-center">Login</div>
+                <div className="text-center">
+                    {isDelegate ? "Delegate " : "Facilitator "}Login
+                </div>
                 <TextInput label="Email" id="email" setState={setFormData} />
                 <TextInput
                     label="Password"
@@ -43,13 +84,22 @@ export default function Login() {
                 <br />
 
                 <div className="text-center">
-                    New to FACT?{" "}
-                    <a
-                        href="/my-fact/register"
-                        className="underline hover:text-highlight-primary"
-                    >
-                        Create an account
-                    </a>
+                    {isDelegate ? (
+                        <>
+                            New to FACT?{" "}
+                            <a
+                                href="/my-fact/register"
+                                className="underline hover:text-highlight-primary"
+                            >
+                                Create an account
+                            </a>
+                        </>
+                    ) : (
+                        <p>
+                            First time logging in? Check your email for detailed
+                            instructions on how to log in.
+                        </p>
+                    )}
                 </div>
             </FormContainer>
         </>
