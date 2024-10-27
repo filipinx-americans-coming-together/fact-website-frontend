1b4269ca15bf3e5a3db40ee5956e90d56f9f8707
Alyssa Daniels <asagedaniels@gmail.com>
2024-10-26 22:23:02 -0500

    fixed workshop card

--- src/components/formatting/WorkshopCard.tsx
+++ src/components/formatting/WorkshopCard.tsx
@@ -2,6 +2,7 @@ import { API_URL } from "@/util/constants";
 import { LocationData, WorkshopData, WorkshopResponse } from "@/util/types";
 import { useEffect, useState } from "react";
 import LoadingCircle from "../icons/LoadingCircle";
+import { useWorkshop } from "@/hooks/api/useWorkshop";
 
 interface WorkshopProps {
     id: number;
@@ -13,59 +14,19 @@ interface WorkshopProps {
  * @returns WorkshopCard component
  */
 export default function WorkshopCard(props: WorkshopProps) {
-    const [workshop, setWorkshop] = useState<WorkshopData>();
-    const [location, setLocation] = useState<LocationData>();
-
-    // workshop data
-    useEffect(() => {
-        fetch(`${API_URL}/workshop/${props.id}/`)
-            .then((response) => {
-                if (response.status === 200) {
-                    response.json().then((data: WorkshopResponse) => {
-                        // workshop data
-                        const workshopData = data.workshop[0];
-
-                        const formattedWorkshop: WorkshopData = {
-                            id: workshopData.pk,
-                            title: workshopData.fields.title,
-                            description: workshopData.fields.description,
-                            facilitators: workshopData.fields.facilitators,
-                            location: workshopData.fields.location,
-                            session: workshopData.fields.session,
-                        };
-
-                        setWorkshop(formattedWorkshop);
-
-                        // location data
-                        const locationData = data.location[0];
-
-                        const formattedLocation: LocationData = {
-                            id: locationData.pk,
-                            room_num: locationData.fields.room_num,
-                            building: locationData.fields.building,
-                            capacity: locationData.fields.capacity,
-                            session: locationData.fields.session,
-                        };
-
-                        setLocation(formattedLocation);
-                    });
-                } else {
-                    response.text().then((text) => console.log(text));
-                }
-            })
-            .catch((e) => console.log(e));
-    }, [props.id]);
+    const { workshop } = useWorkshop({ id: props.id });
 
     return (
         <>
-            {workshop && location ? (
+            {workshop ? (
                 <div className="bg-highlight-primary text-black text-xs px-8 py-4 m-4 rounded-md w-80 shadow-lg">
-                    <div>Session {workshop.session}</div>
+                    <div>Session {workshop.workshop.session}</div>
                     <div className="text-center">
-                        <div>{workshop.title}</div>
+                        <div>{workshop.workshop.title}</div>
                         <div>00:00AM - 00:00AM</div>
                         <div>
-                            {location.building} {location.room_num}
+                            {workshop.location.building}{" "}
+                            {workshop.location.room_num}
                         </div>
                     </div>
                 </div>
