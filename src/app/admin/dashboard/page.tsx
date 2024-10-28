3704339c857a585b876cec602dfe42075fc67183
Alyssa Daniels <asagedaniels@gmail.com>
2024-10-27 21:54:53 -0500

    separated stats and danger zone into components

--- src/app/admin/dashboard/page.tsx
+++ src/app/admin/dashboard/page.tsx
@@ -1,44 +1,13 @@
 "use client";
 
-import TextInput from "@/components/ui/TextInput";
-import { FaTicket, FaTrashCan } from "react-icons/fa6";
-import { FaDownload, FaSchool } from "react-icons/fa";
-import { BsPeopleFill } from "react-icons/bs";
-import RegistrationChart from "./charts/RegistrationChart";
-import TicketsChart from "./charts/TicketsChart";
-import FormContainer from "../components/FormContainer";
-import DateTimeInput from "../components/DateTimeInput";
-import NotificationCard from "../components/NotificationCard";
-import DangerZoneAction from "../components/DangerZoneAction";
 import { useAdminUser } from "@/hooks/api/useAdminUser";
 import ForbiddenPage from "@/components/formatting/ForbiddenPage";
 import Navbar from "../components/Navbar";
 import LoadingCircle from "@/components/icons/LoadingCircle";
 import NotificationManager from "../components/NotificationManager";
-
-const TOGGLEABLE_PERMISSIONS = [
-    {
-        title: "Registration",
-        isEnabled: false,
-        description:
-            "When registration is enabled, delegates are able to create accounts and register for workshops. When registration is disabled, new delegates can not register for workshops, but already registered delegates can change their workshops.",
-        toggle: () => {},
-    },
-    {
-        title: "Automatic Workshop Locations",
-        isEnabled: true,
-        description:
-            "When matching is enabled, workshops are automatically matched to locations based on popularity at 3:00AM CST every Sunday. When matching is disabled, workshop locations will not be changed",
-        toggle: () => {},
-    },
-    {
-        title: "Workshop Changes",
-        isEnabled: true,
-        description:
-            "When workshop changes are enabled, registered delegates can freely change their workshops. When workshop changes are disabled, registered delegates can not change their workshops",
-        toggle: () => {},
-    },
-];
+import Stats from "../components/Stats";
+import { FaDownload } from "react-icons/fa6";
+import DangerZone from "../components/DangerZone";
 
 export default function AdminDashboard() {
     const { user, isLoading } = useAdminUser();
@@ -65,40 +34,7 @@ export default function AdminDashboard() {
                 {/* stats */}
                 <h1 className="text-xl font-bold">At a Glance</h1>
                 <br />
-                <div className="flex flex-col md:flex-row gap-4">
-                    <div className="bg-gradient-to-r from-lime-100 to-green-100 rounded shadow aspect-[4/3] md:w-[240px] flex items-center justify-center">
-                        <div className="flex flex-col gap-2">
-                            <div className="text-4xl text-green-400">
-                                <BsPeopleFill />
-                            </div>
-                            <p className="text-4xl font-bold">234</p>
-                            <p>Registered Delegates</p>
-                        </div>
-                    </div>
-                    <div className="bg-gradient-to-r from-pink-100 to-rose-100 rounded shadow aspect-[4/3] md:w-[240px] flex items-center justify-center">
-                        <div className="flex flex-col gap-2">
-                            <div className="text-4xl text-rose-400">
-                                <FaTicket />
-                            </div>
-                            <p className="text-4xl font-bold">93</p>
-                            <p>Variety Show Tickets</p>
-                        </div>
-                    </div>
-                    <div className="bg-gradient-to-r from-cyan-100 to-blue-100 rounded shadow aspect-[4/3] md:w-[240px] flex items-center justify-center">
-                        <div className="flex flex-col gap-2">
-                            <div className="text-4xl text-blue-400">
-                                <FaSchool />
-                            </div>
-                            <p className="text-4xl font-bold">17</p>
-                            <p>Unique Schools</p>
-                        </div>
-                    </div>
-                </div>
-
-                <br />
-                <RegistrationChart />
-                <br />
-                <TicketsChart />
+                <Stats />
 
                 <br />
                 {/* downloads */}
@@ -122,46 +58,7 @@ export default function AdminDashboard() {
                 {/* danger zone */}
                 <h1 className="text-xl font-bold text-red-600">Danger Zone</h1>
                 <br />
-                <div className="border-2 rounded divide-y-2 flex flex-col">
-                    {TOGGLEABLE_PERMISSIONS.map((permission, idx) => {
-                        return (
-                            <DangerZoneAction
-                                key={idx}
-                                title={permission.title}
-                                description={permission.description}
-                                permissionState={
-                                    permission.isEnabled
-                                        ? "ENABLED"
-                                        : "DISABLED"
-                                }
-                                actionText={
-                                    permission.isEnabled
-                                        ? `Disable ${permission.title}`
-                                        : `Enable ${permission.title}`
-                                }
-                                confirmText={
-                                    permission.isEnabled
-                                        ? `enable ${permission.title.toLowerCase()}`
-                                        : `disable ${permission.title.toLowerCase()}`
-                                }
-                                changePermission={permission.toggle}
-                            />
-                        );
-                    })}
-                    <DangerZoneAction
-                        title="Reset Database"
-                        description="By resetting the database you will remove all workshop, location, facilitator information. Only do this after FACT is over. Account information will not be removed. Resetting the database will require an access key from FACT IT."
-                        actionText={"Reset Database"}
-                        confirmText={
-                            "Are you sure you want to reset the database? This action is permanent and can not be undone."
-                        }
-                        changePermission={() => {
-                            let code = prompt(
-                                "Enter the code provided by another admin"
-                            );
-                        }}
-                    />
-                </div>
+                <DangerZone />
             </div>
             <br />
         </div>
