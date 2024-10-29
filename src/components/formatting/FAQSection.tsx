533b6ac225e9b68ea5501cde7bd929ea6d009cd3
rossebubless <danzemil@icloud.com>
2024-10-28 22:11:52 -0500

    faq updates

--- src/components/formatting/FAQSection.tsx
+++ src/components/formatting/FAQSection.tsx
@@ -7,9 +7,9 @@ interface FAQProps {
 
 export default function FAQSection({question, children}: FAQProps) {
     return (
-        <>
+        <div className="mb-10 w-2/5">
             <div className="text-3xl lg:text-4xl font-bold mb-6">{question}</div>
             <>{children}</>
-        </>
+        </div>
     )
 }
