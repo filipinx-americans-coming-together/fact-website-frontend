6a042338e5255252aa5413bb553a6d72ff12abfd
Alyssa Daniels <asagedaniels@gmail.com>
2024-11-01 17:11:32 -0500

    added missing nav to admin workshops

--- src/app/admin/workshops/page.tsx
+++ src/app/admin/workshops/page.tsx
@@ -6,6 +6,7 @@ import UploadFile from "../components/UploadFile";
 import { useAdminUser } from "@/hooks/api/useAdminUser";
 import LoadingCircle from "@/components/icons/LoadingCircle";
 import ForbiddenPage from "@/components/formatting/ForbiddenPage";
+import Navbar from "../components/Navbar";
 
 export default function Workshops() {
     const { user, isLoading } = useAdminUser();
@@ -25,50 +26,54 @@ export default function Workshops() {
     }
 
     return (
-        <div className="min-h-screen bg-slate-50 text-black">
-            <div className="w-9/12 mx-auto">
-                <h1 className="py-6 text-xl">Workshops</h1>
-                <br />
-                {(workshops && workshops.length) === 0 && (
-                    <UploadFile
-                        title="Workshops"
-                        onUpload={uploadWorkshops}
-                        errorMessage={error?.message}
-                        isLoading={isPending}
-                    />
-                )}
+        <>
+            <Navbar />
+            <div className="min-h-screen bg-slate-50 text-black">
+                <div className="w-9/12 mx-auto">
+                    <h1 className="py-6 text-xl">Workshops</h1>
+                    <br />
+                    {(workshops && workshops.length) === 0 && (
+                        <UploadFile
+                            title="Workshops"
+                            onUpload={uploadWorkshops}
+                            errorMessage={error?.message}
+                            isLoading={isPending}
+                        />
+                    )}
 
-                <br />
+                    <br />
 
-                {[1, 2, 3].map((sessionNum) => {
-                    return (
-                        <div key={sessionNum}>
-                            <h1>Session {sessionNum} Workshops</h1>
-                            <div className="border-t-2" />
-                            <br />
-                            <ul className="flex flex-col gap-2">
-                                {workshops &&
-                                    workshops
-                                        .filter(
-                                            (workshop) =>
-                                                workshop.session == sessionNum
-                                        )
-                                        .map((workshop) => (
-                                            <li
-                                                key={
-                                                    workshop.title +
-                                                    workshop.session
-                                                }
-                                            >
-                                                {workshop.title}
-                                            </li>
-                                        ))}
-                            </ul>
-                            <br />
-                        </div>
-                    );
-                })}
+                    {[1, 2, 3].map((sessionNum) => {
+                        return (
+                            <div key={sessionNum}>
+                                <h1>Session {sessionNum} Workshops</h1>
+                                <div className="border-t-2" />
+                                <br />
+                                <ul className="flex flex-col gap-2">
+                                    {workshops &&
+                                        workshops
+                                            .filter(
+                                                (workshop) =>
+                                                    workshop.session ==
+                                                    sessionNum
+                                            )
+                                            .map((workshop) => (
+                                                <li
+                                                    key={
+                                                        workshop.title +
+                                                        workshop.session
+                                                    }
+                                                >
+                                                    {workshop.title}
+                                                </li>
+                                            ))}
+                                </ul>
+                                <br />
+                            </div>
+                        );
+                    })}
+                </div>
             </div>
-        </div>
+        </>
     );
 }
