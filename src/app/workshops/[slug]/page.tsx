cc38250609f6bd01f647edb4886d8f4a93a2ed47
Kian <kianchristiand@gmail.com>
2024-11-05 13:32:59 -0600

    changed the session page to include facilitator(s) info via modal

--- src/app/workshops/[slug]/page.tsx
+++ src/app/workshops/[slug]/page.tsx
@@ -4,8 +4,9 @@ import { useRouter, useSearchParams } from "next/navigation";
 import PageContainer from "@/components/formatting/PageContainer";
 import Footer from "@/components/formatting/PageFooter";
 import LoadingCircle from "@/components/icons/LoadingCircle";
+import Image from "next/image";
 import { useWorkshop } from "@/hooks/api/useWorkshop";
-import { Suspense } from "react";
+import { Suspense, useState } from "react";
 
 function extractWorkshopId(slug: string) {
     const parts = slug.split("-");
@@ -13,6 +14,82 @@ function extractWorkshopId(slug: string) {
     return parseInt(idPart, 10);
 }
 
+type Facilitator = {
+    id: number;
+    department_name: string;
+    facilitator_names: string[];
+    image_url?: string;
+    position: string;
+    bio: string;
+};
+
+type ModalProps = {
+    children: React.ReactNode;
+    onClose: () => void;
+};
+
+// Modal component to display facilitator details
+const Modal = ({ children, onClose }: ModalProps) => {
+    return (
+        <div
+            className="fixed inset-0 z-50 flex items-center justify-center bg-black bg-opacity-50 md:w-3/4 lg:w-full"
+            onClick={onClose}
+        >
+            <div
+                className="relative bg-white rounded w-full md:w-3/4 max-w-6xl h-[90vh] md:h-auto mx-4"
+            >
+                <button
+                    className="absolute top-2 right-4 text-gray-500 hover:text-gray-800 text-3xl font-bold"
+                    onClick={onClose}
+                >
+                    ×
+                </button>
+                <div className="p-4 overflow-y-auto h-full md:max-h-[80vh]">
+                    {children}
+                </div>
+            </div>
+        </div>
+    );
+};
+
+// FacilitatorDetail component to display in modal
+const FacilitatorDetail = ({ facilitator }: { facilitator: Facilitator }) => {
+    const [isImageLoading, setIsImageLoading] = useState(true);
+
+    return (
+        <div className="relative w-full flex flex-col md:flex-row items-center text-black p-5">
+            <div className="relative w-full h-[70vh] md:w-1/2 lg:w-1/2 lg:h-[60vh] md:h-[45vh]">
+                {isImageLoading && (
+                    <div className="absolute inset-0 flex items-center justify-center bg-gray-200 rounded-md">
+                        <LoadingCircle />
+                    </div>
+                )}
+                {facilitator.image_url && (
+                        <Image
+                            src={facilitator.image_url}
+                            alt={facilitator.department_name}
+                            layout="fill"
+                            fill={true}
+                            className="object-cover text-center rounded-md shadow-lg"
+                            onLoadingComplete={() => setIsImageLoading(false)}
+                            onError={() => setIsImageLoading(false)}
+                        />
+                    )}
+            </div>
+            <div className="md:ml-6 mt-4 md:mt-0 text-left w-full md:w-1/2 p-5">
+                <h2 className="text-2xl md:text-3xl font-bold">
+                    {facilitator.department_name}
+                </h2>
+                {facilitator.position != "nan" && (
+                    <p className="text-sm text-gray-500">{facilitator.position}</p>
+                )}
+                <p className="mt-4">{facilitator.bio}</p>
+            </div>
+        </div>
+    );
+};
+
+// Main content component
 function WorkshopDetailContent({
     params,
 }: {
@@ -26,6 +103,14 @@ function WorkshopDetailContent({
     const { workshop, isLoading, error } = useWorkshop({
         id: workshopId ?? 0,
     });
+
+    const [selectedFacilitator, setSelectedFacilitator] = useState<Facilitator | null>(
+        null
+    );
+
+    const handleFacilitatorClick = (facilitator: any) => {
+        setSelectedFacilitator(facilitator);
+    };
 
     let filterParam = searchParams.get("search");
     let sessionParam = searchParams.get("session");
@@ -46,56 +131,75 @@ function WorkshopDetailContent({
         );
     }
 
-    const { title, description, facilitators, session } = workshop.workshop;
+    const { title, description, session } = workshop.workshop;
     const { building, room_num, capacity } = workshop.location;
+    const facilitators = workshop.facilitators;
 
-    const facilitatorLabel = session === 3 ? "Panelist(s)" : "Facilitator(s)";
+    const facilitatorLabel =
+    session === 3
+        ? facilitators.length === 1
+            ? "Panelist"
+            : "Panelists"
+        : facilitators.length === 1
+        ? "Facilitator"
+        : "Facilitators";
 
     return (
-            <PageContainer title="Workshop">
-                <div className="flex flex-col items-center w-full mx-auto 
-                                p-4 md:p-8 rounded bg-highlight-secondary">
-                    {/** Workshop Title and Description */}
-                    <h1 className="text-3xl font-bold text-center p-2 md:p-4">{title}</h1>
-                    <p className="text-lg text-center px-2 py-12">{description}</p>
+        <PageContainer title="Workshop">
+            <div className="flex flex-col items-center w-full mx-auto 
+                            p-4 md:p-8 rounded bg-highlight-secondary">
+                <h1 className="text-3xl font-bold text-center p-2 md:p-4">{title}</h1>
+                <h1 className="text-2xl font-semibold text-center">Session {session}</h1>
+                <p className="text-lg text-center px-2 py-12">{description}</p>
+                <div className="w-full flex flex-col md:flex-row md:justify-between md:space-x-6 space-y-4 md:space-y-0 text-center">
+                    {/** Facilitator/Panelist Details */}
+                    <div className="flex-1 flex flex-col space-y-2 border-2 rounded p-4">
+                        <h2 className="text-xl font-semibold">{facilitatorLabel}:</h2>
+                        {facilitators.map((facilitator, index) => (
+                            <a
+                                key={index}
+                                onClick={() => handleFacilitatorClick(facilitator)}
+                                className="cursor-pointer underline hover:text-highlight-2-primary transition duration-500"
+                            >
+                                {facilitator.department_name}
+                            </a>
+                        ))}
+                    </div>
 
-                    {/** Workshop and Location Details */}
-                    <div className="w-full flex flex-col md:flex-row md:justify-between md:space-x-6 space-y-4 md:space-y-0 text-center">
-                        {/** Workshop Details */}
-                        <div className="flex-1 flex flex-col space-y-2 border-2 rounded p-4">
-                            <h2 className="text-xl font-semibold">Details</h2>
-                            <p>Session: {session}</p>
-                            <p>{facilitatorLabel}: {facilitators}</p>
-                        </div>
-
-                        {/** Location Details */}
-                        <div className="flex-1 flex flex-col space-y-2 border-2 rounded p-4">
-                            <h2 className="text-xl font-semibold">Location (Subject to Change)</h2>
-                            <p>Building: {building}</p>
-                            <p>Room: {room_num}</p>
-                            <p>Capacity: {capacity}</p>
-                        </div>
+                    {/** Location Details */}
+                    <div className="flex-1 flex flex-col space-y-2 border-2 rounded p-4">
+                        <h2 className="text-xl font-semibold">Location (Subject to Change)</h2>
+                        <p>Building: {building}</p>
+                        <p>Room: {room_num}</p>
+                        <p>Capacity: {capacity}</p>
                     </div>
                 </div>
+            </div>
 
-                {/** Back Button */}
-                <div className="flex justify-center mt-8 px-4">
-                    <button
-                        onClick={() => {
-                            router.push(`/workshops?search=${filterParam}&session=${sessionParam}`)
-                        }}
-                        className="w-full md:w-auto px-4 py-2 bg-highlight-secondary 
+            <div className="flex justify-center mt-8 px-4">
+                <button
+                    onClick={() => {
+                        router.push(`/workshops?search=${filterParam}&session=${sessionParam}`);
+                    }}
+                    className="w-full md:w-auto px-4 py-2 bg-highlight-secondary 
                                 text-white rounded-md hover:bg-highlight-primary 
                                 transition duration-500"
-                    >
-                        &larr; Back
-                    </button>
-                </div>
-                <Footer />
-            </PageContainer>
+                >
+                    &larr; Back
+                </button>
+            </div>
+
+            {selectedFacilitator && (
+                <Modal onClose={() => handleFacilitatorClick(null)}>
+                    <FacilitatorDetail facilitator={selectedFacilitator} />
+                </Modal>
+            )}
+            <Footer />
+        </PageContainer>
     );
 }
 
+// Main component
 export default function WorkshopDetail({ params }: { params: { slug: string } }) {
     return (
         <Suspense fallback={<LoadingCircle />}>
