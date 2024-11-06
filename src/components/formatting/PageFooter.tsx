65606d748df51146c58e91c4bba4737b2f0c1705
Kian <kianchristiand@gmail.com>
2024-11-05 19:55:03 -0600

    added PSA logo to the footer

--- src/components/formatting/PageFooter.tsx
+++ src/components/formatting/PageFooter.tsx
@@ -2,8 +2,9 @@ import Image from "next/image";
 import Link from "next/link";
 
 export const Footer = () => {
-    const FACT_LOGO_LENGTH = 100;
+    const FACT_LOGO_LENGTH = 60;
     const FACT_LOGO_SOURCE = "/fact-2024-logo.png";
+    const PSA_LOGO_SOURCE = "/psa-logo.jpg";
 
     return (
         <footer className="pt-12 pb-6 px-4 w-full flex flex-col items-center">
@@ -12,9 +13,16 @@ export const Footer = () => {
                 <span className="hidden sm:block flex-grow" />
                 <Image
                     src={FACT_LOGO_SOURCE}
-                    width={FACT_LOGO_LENGTH - 40}
-                    height={FACT_LOGO_LENGTH - 40}
+                    width={FACT_LOGO_LENGTH}
+                    height={FACT_LOGO_LENGTH}
                     alt="FACT 2024 Logo"
+                    className="object-contain rounded-lg"
+                />
+                <Image
+                    src={PSA_LOGO_SOURCE}
+                    width={FACT_LOGO_LENGTH}
+                    height={FACT_LOGO_LENGTH}
+                    alt="PSA Logo"
                     className="object-contain rounded-lg"
                 />
             </div>
