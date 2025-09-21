8ec81be991c9ffb78e831f038c68b29ad2c8b7cb
samev <samevangelista07@icloud.com>
2025-09-21 18:32:33 -0500

    About Page

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -192,7 +192,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "28193a3736699e74"; }
+/******/ 		__webpack_require__.h = function() { return "169c32c289829617"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
