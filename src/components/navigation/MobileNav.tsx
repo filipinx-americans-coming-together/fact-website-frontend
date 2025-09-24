7e5c62f49cb5cfcd67d8454fed3cdae3dc33b655
Emilia Daniels <danzemil@icloud.com>
2025-09-24 16:37:28 -0500

    mobile navbar fixes about/gallery format fixes

--- src/components/navigation/MobileNav.tsx
+++ src/components/navigation/MobileNav.tsx
@@ -4,7 +4,7 @@ export default function MobileNav(props: {
     links: { text: string; path: string }[];
 }) {
     return (
-        <ul className="absolute z-10 right-0 rounded-sm py-2 mt-2 text-left flex flex-col min-w-48 text-background-primary bg-text-primary border border-black">
+        <ul className="absolute z-20 right-0 rounded-sm py-2 mt-2 text-left flex flex-col min-w-48 bg-highlight-primary border border-black">
             {props.links.map((link, index) => (
                 <Link
                     className="px-6 py-2 hover:bg-slate-200"
