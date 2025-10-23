327d93027ea8d1e8b307346d1d777bf3c7cdd08b
samev <samevangelista07@icloud.com>
2025-10-22 20:57:54 -0500

    Variety Show Page

--- src/app/vshow/page.tsx
+++ src/app/vshow/page.tsx
@@ -4,9 +4,11 @@ export default function VShowPage() {
     return (
         <PageContainer title="Variety Show" background="bg-gradient mask-(--background-image-blurry-3) mask-size-[1400px] mask-top">
             <img/>
-            <h1>Headliner</h1>
+            <h1>FACT Headliner</h1>
 
             <h2>Performers</h2>
+
+            
         </PageContainer>
     );
 }
